ab7ce8d1d29232108ae2466f1043c0f60e6b0af3
ieatrice69420 <ajwangehs@gmail.com>
2025-06-01 19:44:02 -0500

    Added Growing, Jumping
    TODO: Apples, Death

---
+++ Crazy Worm 3D/Assets/Scripts/BodySegment.cs
@@ -0,0 +1,34 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BodySegment : MonoBehaviour
+{
+    private Vector3 currentPosition, previousPosition;
+    private int delay;
+    private Queue<Vector3> positionHistory = new Queue<Vector3>();
+    public Vector3 lastPositionSinceDelay;
+
+    private void Start()
+    {
+        currentPosition = transform.position;
+        lastPositionSinceDelay = transform.position;
+        delay = FindAnyObjectByType<Movement>().delay;
+
+        for (int i = 0; i < delay; i++)
+        {
+            positionHistory.Enqueue(currentPosition);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        positionHistory.Enqueue(previousPosition);
+        previousPosition = currentPosition;
+        currentPosition = transform.position;
+
+        if (positionHistory.Count > delay)
+        {
+            lastPositionSinceDelay = positionHistory.Dequeue();
+        }
+    }
+}
--- Crazy Worm 3D/Assets/Scripts/Movement.cs
+++ Crazy Worm 3D/Assets/Scripts/Movement.cs
@@ -1,45 +1,83 @@
-using UnityEditor.U2D;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    public float moveSpeed, steerSpeed, jumpForce, gravity;
-    private CharacterController myCharacterController;
-    private float jumpVelocity;
+    public float moveSpeed, steerSpeed, jumpForce, groundDistance;
+    public int startingLength, delay;
+    public GameObject bodySegmentPrefab;
+    public LayerMask groundLayer;
+    public Transform groundCheckPoint;
+    private Rigidbody myRigidbody;
+    private List<BodySegment> myBodySegments = new List<BodySegment>();
+
 
     void Start()
     {
-        myCharacterController = GetComponent<CharacterController>();
+        myRigidbody = GetComponent<Rigidbody>();
+        myBodySegments.Add(GetComponent<BodySegment>());
+
+        for (int i = 0; i < startingLength - 1; i++)
+        {
+            Grow();
+        }
     }
 
     private void Update()
     {
+        if (PlayerIsGrounded() && Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            Jump();
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Grow();
+        }
+    }
+
+    private bool PlayerIsGrounded()
+    {
+        // OnDrawGizmos();
+        return Physics.CheckSphere(groundCheckPoint.position, groundDistance, groundLayer);
+    }
+
+
+    private void FixedUpdate()
+    {
+        UpdateBodySegments();
         Move();
     }
 
     private void Jump()
     {
-        if (myCharacterController.isGrounded)
-        {
-            if (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
-            {
-                jumpVelocity = jumpForce;
-            }
-        }
-        else
-        {
-            jumpVelocity -= gravity * Time.deltaTime;
-        }
+        myRigidbody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
     }
 
     private void Move()
     {
         float steerDirection = Input.GetAxis("Horizontal");
-        transform.Rotate(Vector3.up * steerDirection * steerSpeed * Time.deltaTime);
+        transform.Rotate(Vector3.up * steerDirection * steerSpeed * Time.fixedDeltaTime);
 
-        Jump();
+        Vector3 horizontalMovement = transform.forward * moveSpeed * Time.fixedDeltaTime;
+        myRigidbody.MovePosition(transform.position + horizontalMovement);
+    }
 
-        Vector3 horizontalMovement = transform.forward * moveSpeed * Time.deltaTime;
-        myCharacterController.Move(horizontalMovement + Vector3.up * jumpVelocity);
+    private void UpdateBodySegments()
+    {
+        for (int i = 1; i < myBodySegments.Count; i++)
+        {
+            BodySegment currentSegment = myBodySegments[i];
+            BodySegment previousSegment = myBodySegments[i - 1];
+
+            currentSegment.transform.LookAt(previousSegment.lastPositionSinceDelay);
+            currentSegment.transform.position = previousSegment.lastPositionSinceDelay;
+        }
+    }
+
+    private void Grow()
+    {
+        GameObject instantiatedBodySegment = Instantiate(bodySegmentPrefab, transform.position, transform.rotation);
+        myBodySegments.Add(instantiatedBodySegment.GetComponent<BodySegment>());
     }
 }
